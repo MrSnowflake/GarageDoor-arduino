4add96cbb4d950d261222cbb60333a07e6fc47da
Maarten Krijn <Maarten.Krijn@amplexor.com>
2017-02-18 14:17:54 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include "settings.h"
+
+ESP8266WebServer server(80);
+
+void setup() {
+    WiFi.
+}
+
+void loop() {
+
+}
