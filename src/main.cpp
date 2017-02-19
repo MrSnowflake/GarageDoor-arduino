b6d6fdb4f96a63dca9b9bd66bd8a70a4e46182d0
Maarten Krijn <Maarten.Krijn@amplexor.com>
2017-02-19 10:34:46 +0100

    ATO Working

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #include "settings.h"
 
+const char* VERSION = "v1.1";
+
 ESP8266WebServer server(80);
 
 const int TOGGLE = D1;
@@ -30,10 +32,11 @@ void handleRoot() {
   <body>\
     <h1>GarageDoor</h1>\
     <p>Uptime: %02d:%02d:%02d</p>\
+    <p>%s</p>\
   </body>\
 </html>",
 
-		hr, min % 60, sec % 60
+		hr, min % 60, sec % 60, VERSION
 	);
 	server.send(200, "text/html", temp);
 }
@@ -47,6 +50,8 @@ void handleNotFound() {
 void setup() {
 	pinMode(TOGGLE, OUTPUT);
 	pinMode(STATUS_LED, OUTPUT);
+
+	digitalWrite(STATUS_LED, LOW);
 
 	Serial.begin(115200);
 
@@ -62,7 +67,6 @@ void setup() {
 
 		digitalWrite(STATUS_LED, statusLedStatus = !statusLedStatus);
 	}
-
 	digitalWrite(STATUS_LED, LOW);
 
 	Serial.println("");
@@ -98,7 +102,25 @@ void setup() {
 	server.begin();
 	Serial.println("HTTP server started");
 
+	ArduinoOTA.onStart([]() {
+		Serial.println("Start");
+	});
+	ArduinoOTA.onEnd([]() {
+		Serial.println("\nEnd");
+	});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+	});
+	ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+		else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
 	ArduinoOTA.begin();
+	digitalWrite(STATUS_LED, HIGH);
 }
 
 void loop() {
